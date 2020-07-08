a8b14b5fbdd994b1a63f35bde045f7d4d9c9fa20
Kristian Kielhofner <kris@kriskinc.com>
2020-07-08 14:22:11 -0500

    Add targets for decoders and kenlm

--- buildkit/bc.sh
+++ buildkit/bc.sh
@@ -5,5 +5,5 @@ buildctl build \
     --local context=.. \
     --progress plain \
     --local dockerfile=. \
-    --opt target=nemo \
-    --output type=docker,name=nemo-stable:0.10.1r2 | docker load
+    --opt target=kenlm \
+    --output type=docker,name=nemo-stable:0.10.1r4 | docker load
